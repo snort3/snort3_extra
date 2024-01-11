b2848bfe871cd5b07625bcd27fa612a5e7f47bde
Maya Dagon (mdagon) <mdagon@cisco.com>
2024-01-11 13:37:10 +0000

    Pull request #172: Copyright: update year to 2024
    Merge in SNORT/snort3_extra from ~MDAGON/snort3_extra:copyright to master
    
    Squashed commit of the following:
    
    commit 49aa952037e4c8c79116de90eec4cde7fb26a2eb
    Author: maya dagon <mdagon@cisco.com>
    Date:   Tue Jan 9 10:58:57 2024 -0500
    
        Copyright: update year to 2024

--- src/search_engines/lowmem/sfksearch.cc
+++ src/search_engines/lowmem/sfksearch.cc
@@ -1,6 +1,6 @@
 //--------------------------------------------------------------------------
 // Copyright (C) 2001 Marc Norton
-// Copyright (C) 2014-2023 Cisco and/or its affiliates. All rights reserved.
+// Copyright (C) 2014-2024 Cisco and/or its affiliates. All rights reserved.
 // Copyright (C) 2003-2013 Sourcefire, Inc.
 //
 // This program is free software; you can redistribute it and/or modify it
